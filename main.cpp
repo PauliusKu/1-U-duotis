7013e9ac4a9125266eac56ed69b07f37cc847986
PauliusKu <paulius.kuprevicius@gmail.com>
2018-02-08 12:28:15 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string Vardas;
+    char z = '*';
+    cout << "Iveskite savo varda: ";
+    cin >> Vardas;
+    std::cout << "The size of str is " << Vardas.length() << " bytes.\n";
+
+    cout << "Hello world!" << endl;
+    cin >> Vardas;
+    return 0;
+}
