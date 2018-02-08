964ebca7a235a9b3858732e2ab248fb4a5a6d75d
PauliusKu <paulius.kuprevicius@gmail.com>
2018-02-08 12:42:51 +0200

    Remelis

--- main.cpp
+++ main.cpp
@@ -11,6 +11,34 @@ int main()
     cin >> Vardas;
     std::cout << "The size of str is " << Vardas.length() << " bytes.\n";
 
+    for (int i = 0; i < 5; i++)
+    {
+        if (i%2==0)
+        {
+            if(i == 2)
+            {
+                cout << z << Vardas << z << endl;
+
+            } else
+            {
+                for (int i = 0; i < Vardas.length()+2; i++)
+                {
+                    cout << z;
+                }
+                cout << endl;
+            }
+        } else
+        {
+            cout << z;
+            for (int i = 0; i < Vardas.length(); i++)
+            {
+                cout << " ";
+            }
+            cout << z << endl;
+        }
+
+    }
+
     cout << "Hello world!" << endl;
     cin >> Vardas;
     return 0;
